9db67da7a84c6fda81e88424b5e388bcc3fd3275
florentinobenedictus <florentinobenedictus@gmail.com>
2022-10-25 22:36:09 +0700

    no 10-13, no 14 bug

--- node/scriptEden.sh
+++ node/scriptEden.sh
@@ -19,7 +19,7 @@ wait
 
 cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/wise.E07.com.conf
 
-echo '<VirtualHost *:8080>
+echo '<VirtualHost *:80>
 
         ServerName wise.E07.com
         ServerAlias www.wise.E07.com
@@ -52,7 +52,8 @@ echo '# If you just change the port or add more ports here, you will likely also
 # /etc/apache2/sites-enabled/000-default.conf
 
 Listen 80
-Listen 8080
+Listen 15000
+Listen 15500
 
 <IfModule ssl_module>
         Listen 443
@@ -71,8 +72,93 @@ wait $!
 mv /var/www/wise /var/www/wise.E07.com
 rm wise.zip
 
+
+
+wget --no-check-certificate "https://drive.google.com/uc?id=1q9g6nM85bW5T9f5yoyXtDqonUKKCHOTV" -O eden.wise.zip &
+wait $!
+unzip /root/eden.wise.zip -d /var/www &
+wait $!
+mv /var/www/eden.wise /var/www/eden.wise.E07.com
+rm eden.wise.zip
+
+echo '<VirtualHost *:80>
+
+        ServerName eden.wise.E07.com
+        ServerAlias www.eden.wise.E07.com
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/eden.wise.E07.com
+
+        <Directory /var/www/eden.wise.E07.com/public>
+                Options +Indexes
+        </Directory>
+
+        # <Directory /var/www/eden.wise.E07.com/public/*>
+        #         Options -Indexes
+        # </Directory>
+
+        ErrorDocument 404 /error/404.html
+
+        Alias "/js" "/var/www/eden.wise.E07.com/public/js"
+
+        # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+        # error, crit, alert, emerg.
+        # It is also possible to configure the loglevel for particular
+        # modules, e.g.
+        #LogLevel info ssl:warn
+        
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+        # For most configuration files from conf-available/, which are
+        # enabled or disabled at a global level, it is possible to
+        # include a line for only one particular virtual host. For example the
+        # following line enables the CGI configuration for this host only
+        # after it has been globally disabled with "a2disconf".
+        #Include conf-available/serve-cgi-bin.conf
+</VirtualHost>
+
+# vim: syntax=apache ts=4 sw=4 sts=4 sr noet' > /etc/apache2/sites-available/eden.wise.E07.com.conf
+
+
+wget --no-check-certificate "https://drive.google.com/uc?export=download&id=1bgd3B6VtDtVv2ouqyM8wLyZGzK5C9maT" -O strix.operation.wise.zip &
+wait $!
+unzip /root/strix.operation.wise.zip -d /var/www &
+wait $!
+mv /var/www/strix.operation.wise /var/www/strix.operation.wise.E07.com
+rm strix.operation.wise.zip
+
+echo '<VirtualHost *:15000 *:15500>
+
+        ServerName strix.operation.wise.E07.com
+        ServerAlias www.strix.operation.wise.E07.com
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/strix.operation.wise.E07.com
+
+        # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+        # error, crit, alert, emerg.
+        # It is also possible to configure the loglevel for particular
+        # modules, e.g.
+        #LogLevel info ssl:warn
+        
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+        # For most configuration files from conf-available/, which are
+        # enabled or disabled at a global level, it is possible to
+        # include a line for only one particular virtual host. For example the
+        # following line enables the CGI configuration for this host only
+        # after it has been globally disabled with "a2disconf".
+        #Include conf-available/serve-cgi-bin.conf
+</VirtualHost>
+
+# vim: syntax=apache ts=4 sw=4 sts=4 sr noet' > /etc/apache2/sites-available/strix.operation.wise.E07.com.conf
+
+
+
 a2dissite 000-default.conf 
 a2ensite wise.E07.com.conf
+a2ensite eden.wise.E07.com.conf
+a2ensite strix.operation.wise.E07.com.conf
 service apache2 restart
 
 
--- node/scriptSSS.sh
+++ node/scriptSSS.sh
@@ -1,3 +1,5 @@
+# IP Ping tidak sesuai? nano /etc/resolv.conf langsung tutup lagi
+
 echo 'nameserver 10.25.2.2 # IP WISE
 nameserver 10.25.3.2 # IP Berlint
 nameserver 192.168.122.1' > /etc/resolv.conf
