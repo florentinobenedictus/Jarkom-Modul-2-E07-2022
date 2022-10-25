b9c734215bd7c0f6fdaea28dd930bce06abc9bee
florentinobenedictus <florentinobenedictus@gmail.com>
2022-10-25 15:16:48 +0700

    fix ns

--- node/scriptEden.sh
+++ node/scriptEden.sh
@@ -1,3 +1,11 @@
+echo 'nameserver 192.168.122.1
+nameserver 10.25.2.2 # IP WISE
+nameserver 10.25.3.2 # IP Berlint' > /etc/resolv.conf
+
+apt-get update
+apt-get install apache2 -y
+
 echo 'nameserver 10.25.2.2 # IP WISE
 # nameserver 10.25.3.2 # IP Berlint
 nameserver 192.168.122.1' > /etc/resolv.conf
+
